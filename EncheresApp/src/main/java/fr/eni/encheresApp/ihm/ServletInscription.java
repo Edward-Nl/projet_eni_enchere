bba8de3e421079ad6fa10e8653c92f3f5ba25153
baptiste <baptiste.tielles@outlook.fr>
2022-02-02 16:02:04 +0100

    Rectification Ergonomie ServletInscription

--- EncheresApp/src/main/java/fr/eni/encheresApp/ihm/ServletInscription.java
+++ EncheresApp/src/main/java/fr/eni/encheresApp/ihm/ServletInscription.java
@@ -34,45 +34,46 @@ public class ServletInscription extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		UtilisateurManager manager = new UtilisateurManager();
-
-		String pseudo = request.getParameter("pseudo");
-		String prenom = request.getParameter("prenom");
-		String nom = request.getParameter("nom");
-		String email = request.getParameter("email");
-		String telephone = request.getParameter("telephone");
-		String rue = request.getParameter("rue");
-		String codePostale = request.getParameter("codePostale");
-		String ville = request.getParameter("ville");
-
+		Utilisateur utilisateur = null;
 		String mdp = request.getParameter("mdp");
 		String mdpC = request.getParameter("mdpC");
 
 		boolean mdpValid = valideMdp(mdp, mdpC);
 
 		if (mdpValid) {
+			UtilisateurManager manager = new UtilisateurManager();
+			String pseudo = request.getParameter("pseudo");
+			String email = request.getParameter("email");
 			if (!manager.selectByMailOrPseudo(email, pseudo)) {
+				String prenom = request.getParameter("prenom");
+				String nom = request.getParameter("nom");
+				String telephone = request.getParameter("telephone");
+				String rue = request.getParameter("rue");
+				String codePostale = request.getParameter("codePostale");
+				String ville = request.getParameter("ville");
 				// TODO: changer le system crédit & amdin
-				Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostale,
-						ville, CryptagePassword.crypteString(mdp), 500, false);
+				utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostale, ville,
+						CryptagePassword.crypteString(mdp), 500, false);
 				manager.ajouterUtilisateur(utilisateur);
-				// TODO: SESSION utilisateur
-				// TODO redirection vers page acceuil
+
 			}
 
 		}
+		if (utilisateur != null) {
+			request.getSession().setAttribute("utilisateurCourant", utilisateur);
+			response.sendRedirect(request.getContextPath() + "/");
+		} else {
+			request.setAttribute("pseudo", request.getParameter("pseudo"));
+			request.setAttribute("prenom", request.getParameter("prenom"));
+			request.setAttribute("nom", request.getParameter("nom"));
+			request.setAttribute("email", request.getParameter("email"));
+			request.setAttribute("telephone", request.getParameter("telephone"));
+			request.setAttribute("rue", request.getParameter("rue"));
+			request.setAttribute("codePostale", request.getParameter("codePostale"));
+			request.setAttribute("ville", request.getParameter("ville"));
 
-		request.setAttribute("pseudo", pseudo);
-		request.setAttribute("prenom", prenom);
-		request.setAttribute("nom", nom);
-		request.setAttribute("email", email);
-		request.setAttribute("telephone", telephone);
-		request.setAttribute("rue", rue);
-		request.setAttribute("codePostale", codePostale);
-		request.setAttribute("ville", ville);
-
-		doGet(request, response);
-
+			doGet(request, response);
+		}
 	}
 
 	private boolean valideMdp(String mdp, String mdpC) {
