a31ca716eb39d78eab88aabf829877c913fcf3d7
baptiste <baptiste.tielles@outlook.fr>
2022-02-02 12:38:11 +0100

    mise au propre SerbletModifier

--- EncheresApp/src/main/java/fr/eni/encheresApp/ihm/ServletModifierProfil.java
+++ EncheresApp/src/main/java/fr/eni/encheresApp/ihm/ServletModifierProfil.java
@@ -25,12 +25,12 @@ public class ServletModifierProfil extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		UtilisateurManager manager = new UtilisateurManager();
-		Utilisateur utilisateur = manager.selectAvecId(1);
-		HttpSession session = request.getSession();
-		session.setAttribute("utilisateur", utilisateur);
+		if (request.getSession().getAttribute("utilisateurCourant") != null) {
+			request.getRequestDispatcher("/WEB-INF/views/jspModifierProfil.jsp").forward(request, response);
+		} else {
+			response.sendRedirect(request.getContextPath() + "/ServletAccueil");
+		}
 
-		request.getRequestDispatcher("/WEB-INF/views/jspModifierProfil.jsp").forward(request, response);
 	}
 
 	/**
@@ -40,48 +40,64 @@ public class ServletModifierProfil extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		UtilisateurManager manager = new UtilisateurManager();
-		HttpSession session = request.getSession();
-		Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateur");
-		String pseudo = request.getParameter("pseudo").trim();
-		String nom = request.getParameter("nom").trim();
-		String prenom = request.getParameter("prenom").trim();
-		String email = request.getParameter("email").trim();
-		String telephone = request.getParameter("telephone").trim();
-		String rue = request.getParameter("rue").trim();
-		String code_postal = request.getParameter("codePostal").trim();
-		String ville = request.getParameter("ville").trim();
-		String mdp = request.getParameter("mdp").trim();
-		String mdpC = request.getParameter("mdpC").trim();
-		String mdpO = request.getParameter("mdpO").trim();
+		if (request.getSession().getAttribute("utilisateurCourant") != null) {
+			HttpSession session = request.getSession();
+			Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateurCourant");
 
-		if (manager.selectByIdAndPassword(utilisateur.getNoUtilisateur(), CryptagePassword.crypteString(mdpO))) {
-			if (!mdp.isEmpty() || !mdpC.isEmpty()) {
-				if (mdp.equals(mdpC)) {
-					String motDePasse = CryptagePassword.crypteString(mdp);
+			String mdpO = request.getParameter("mdpO").trim();
+
+			if (manager.selectByIdAndPassword(utilisateur.getNoUtilisateur(), CryptagePassword.crypteString(mdpO))) {
+				String mdp = request.getParameter("mdp").trim();
+				String mdpC = request.getParameter("mdpC").trim();
+				String motDePasse = null;
+
+				if (!mdp.isEmpty() || !mdpC.isEmpty()) {
+					if (mdp.equals(mdpC)) {
+						motDePasse = CryptagePassword.crypteString(mdp);
+						String pseudo = request.getParameter("pseudo").trim();
+						String nom = request.getParameter("nom").trim();
+						String prenom = request.getParameter("prenom").trim();
+						String email = request.getParameter("email").trim();
+						String telephone = request.getParameter("telephone").trim();
+						String rue = request.getParameter("rue").trim();
+						String code_postal = request.getParameter("codePostal").trim();
+						String ville = request.getParameter("ville").trim();
+
+						Utilisateur tmpUtilisateur = new Utilisateur(utilisateur.getNoUtilisateur(), pseudo, nom,
+								prenom, email, telephone, rue, code_postal, ville, motDePasse, utilisateur.getCredit(),
+								utilisateur.isAdministrateur());
+						if (!utilisateur.equals(tmpUtilisateur)) {
+							if (manager.updateUtilisateur(tmpUtilisateur)) {
+								session.setAttribute("utilisateurCourant", tmpUtilisateur);
+							}
+						}
+					}
+
+				} else {
+					motDePasse = CryptagePassword.crypteString(mdpO);
+					String pseudo = request.getParameter("pseudo").trim();
+					String nom = request.getParameter("nom").trim();
+					String prenom = request.getParameter("prenom").trim();
+					String email = request.getParameter("email").trim();
+					String telephone = request.getParameter("telephone").trim();
+					String rue = request.getParameter("rue").trim();
+					String code_postal = request.getParameter("codePostal").trim();
+					String ville = request.getParameter("ville").trim();
+
 					Utilisateur tmpUtilisateur = new Utilisateur(utilisateur.getNoUtilisateur(), pseudo, nom, prenom,
 							email, telephone, rue, code_postal, ville, motDePasse, utilisateur.getCredit(),
 							utilisateur.isAdministrateur());
 					if (!utilisateur.equals(tmpUtilisateur)) {
 						if (manager.updateUtilisateur(tmpUtilisateur)) {
-							session.setAttribute("utilisateur", tmpUtilisateur);
+							session.setAttribute("utilisateurCourant", tmpUtilisateur);
 						}
 					}
 				}
-			} else {
-				String motDePasse = CryptagePassword.crypteString(mdpO);
-				Utilisateur tmpUtilisateur = new Utilisateur(utilisateur.getNoUtilisateur(), pseudo, nom, prenom, email,
-						telephone, rue, code_postal, ville, motDePasse, utilisateur.getCredit(),
-						utilisateur.isAdministrateur());
-				if (!utilisateur.equals(tmpUtilisateur)) {
-					if (manager.updateUtilisateur(tmpUtilisateur)) {
-						session.setAttribute("utilisateur", tmpUtilisateur);
-					}
-				}
 			}
-
+			doGet(request, response);
+		} else {
+			response.sendRedirect(request.getContextPath() + "/ServletAccueil");
 		}
 
-		doGet(request, response);
 	}
-
 }
